8f22de331fceb8b230613c856cfbf6b34dae4c22
marcustzeng <jiunfengtzeng@gmail.com>
2024-02-06 15:21:00 +0800

    feat:stores/userinfo 填充測試資料

--- vue-main/src/main.js
+++ vue-main/src/main.js
@@ -2,12 +2,19 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-
+import { useUserInfoStore } from "@/stores/userInfo";
 import App from './App.vue'
 import router from './router'
 
-const app = createApp(App)
-app.use(createPinia())
+const app = createApp(App);
+app.use(createPinia());
+const userStore = useUserInfoStore();
+app.config.globalProperties.userStore = userStore;
+userStore._setTestAccount()//預塞測試帳號資料
+
+
+
+
 
 app.use(router)
 
