c3b4f8eb7e4dd6315201869f8482d91dfec5aaab
marcustzeng <jiunfengtzeng@gmail.com>
2024-02-22 10:21:18 +0800

    feat:api 1.帳號登入與創建api

--- express-api/app.js
+++ express-api/app.js
@@ -1,30 +1,107 @@
-
+//所需的npm包
 const express = require('express');
+const mysql = require('mysql')
+const bodyParser = require('body-parser');
 
 const app = express();
 
-const users = [
-    { id: 1, name: 'Alice', email: 'alice@example.com' },
-    { id: 2, name: 'Bob', email: 'bob@example.com' },
-    { id: 3, name: 'Charlie', email: 'charlie@example.com' }
-];
+//db連線程序設定
+const connection = mysql.createConnection({
+    host: 'localhost',
+    port: '3306',
+    user: 'root',
+    password: '123456',
+    database: 'projectb'
+})
 
+//連線db
+connection.connect((err) => {
+    if (err) {
+        console.error('Error connection to database: ', err);
+        return
+    }
+    console.log('Connected to database successfully');
+})
+
+app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
-    res.send('Hello, Express!');
+    res.json({ message: 'API server is living' })
 });
 
-app.get('/user/:id', (req, res) => {
-    const userId = parseInt(req.params.id);
-    const user = users.find(user => user.id === userId);
-    if (user) {
-        res.json(user); // 返回用户数据
-    } else {
-        res.status(404).json({ error: 'User not found' }); // 返回用户不存在的错误
-    }
+//使用者登入
+app.post('/userLogin', (req, res) => {
+    const { account, password } = req.body;
+
+    //檢查帳號是否存在以及密碼是否正確
+    connection.query('SELECT * FROM user_account WHERE account = ?', [account], (error, results) => {
+        if (error) {
+            console.error('錯誤查詢:', error);
+
+        }
+        //檢查用戶是否存在或者密碼錯誤
+        if (results.length > 0 && password == results[0].password) {
+            res.json(results)
+        } else {
+            res.json({ message: '帳號或密碼錯誤' })
+        }
+
+    })
+
 });
 
-const PORT = process.env.PORT || 3000;
+//註冊使用者
+app.post('/userCreate', (req, res) => {
+    const { account, password, username } = req.body;
+
+    //檢查帳號是否存在
+    connection.query('SELECT * FROM user_account WHERE account = ?', [account], (error, results) => {
+        if (error) {
+            console.error('錯誤查詢:', error)
+            return res.json({ message: '發生異常錯誤，帳號無法創建。' });
+        }
+        if (results.length > 0) {
+            return res.json({ message: '該帳號已有人使用' })
+        } else {//新增帳號
+
+            //預設新帳號內容物
+            const newAccountContain = {
+                account,
+                password,
+                username,
+                rank: 1,
+                money: 1000,
+                credit: 500,
+                experience: 100,
+                pet_team: "001|002|003",
+                items: "001,20|002,10|004,10"
+            }
+
+            const insertQuery = 'INSERT INTO user_account SET ?';
+            const insertValues = [JSON.stringify(newAccountContain)]
+
+            connection.query(insertQuery, newAccountContain, (error, results) => {
+                if (error) {
+                    console.error('Error insert data', error);
+                    return res.json({ message: '帳號創建失敗' })
+                }
+                console.log('Data inserted successfully');
+                return res.json({ message: '帳號創建完成' })
+            })
+
+            // const addUser = 'INSERT INTO user_account (account,password) VALUES (?,?)'
+            // const values = [account,password]
+
+        }
+    })
+
+
+
+})
+
+
+//監聽
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
