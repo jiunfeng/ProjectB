3008828f1faa04eda63c0083abac36c9c5b116fc
hsd325 <fang91258@gmail.com>
2024-02-22 14:59:13 +0800

    登入頁面已排版完成

--- vue-main/src/components/mainui/IntoPage.vue
+++ vue-main/src/components/mainui/IntoPage.vue
@@ -2,29 +2,29 @@
     <div class="body1 mx-auto">
         <!-- 灰色內容 -->
         <div class="box bg1 translate-middle">
-            <img src="/mainui/images/into/welcome.png" alt="">
+            <img class="welcome translate-middle" src="/mainui/images/into/welcome.png" alt="">
             <!-- 帳號 -->
-            <label for="in1" class="inbox2 text-white">帳號 :</label>
-            <div>
-                <input type="text" class="inbox3 form-control is-invalid" name="in1" id="in1" placeholder="請輸入5~8個之間的英文數字"
-                    oninput="value=value.replace(/[^\w\.\/]/ig,'')">
+            <label for="in1" class="text1 text-white">帳號 :</label>
+            <div class="inputdiv1">
+                <input type="text" class="input1 form-control is-invalid" name="in1" id="in1" placeholder="請輸入5~8個之間的英文數字"
+                    oninput="value=value.replace(/[^\w\.\/]/ig,'')" style="font-size: 20px;">
                 <!-- 只能輸入數字和英文 -->
-                <div class="valid-feedback inbox3-1">字數符合</div>
-                <div class="invalid-feedback inbox3-2" id="inva01">字數請在5~8之內</div>
-                <div class="invalid-feedback inbox3-3 d-none" id="inva02">帳號已存在</div>
+                <div class="valid-feedback input1-1" style="font-size: 25px;">&nbsp;字數符合</div>
+                <div class="invalid-feedback input1-2" id="inva01" style="font-size: 25px;">&nbsp;字數請在5~8之內</div>
+                <div class="invalid-feedback input1-3 d-none" id="inva02" >&nbsp;帳號已存在</div>
             </div>
             <!-- 密碼 -->
-            <label for="in2" class="inbox4 text-white">密碼 :</label>
-            <div>
-                <input type="password" class="inbox5 form-control is-invalid" name="in2" id="in2"
-                    placeholder="請輸入1~8個之間的英文數字" oninput="value=value.replace(/[^\w\.\/]/ig,'')">
-                <div class="inbox5-1 valid-feedback">字數符合</div>
-                <div class="inbox5-2 invalid-feedback" id="inva01">字數請在5~8之內</div>
+            <label for="in2" class="text2 text-white">密碼 :</label>
+            <div class="inputdiv2">
+                <input type="password" class="input2 form-control is-invalid" name="in2" id="in2"
+                    placeholder="請輸入5~8個之間的英文數字" oninput="value=value.replace(/[^\w\.\/]/ig,'')" style="font-size: 20px;">
+                <div class="input2-1 valid-feedback" style="font-size: 25px;">&nbsp;字數符合</div>
+                <div class="input2-2 invalid-feedback" id="inva01" style="font-size: 25px;">&nbsp;字數請在5~8之內</div>
             </div>
             <!-- 登入按鈕 -->
-            <input class="inbox8 border border-success" type="image" src="https://fakeimg.pl/50x50/200/?text=change" alt=""
-                name="" id="" onclick="send();" />
             <button class="btn1 border border-success" type="button"></button>
+            <!-- 註冊按鈕 -->
+            <button class="btn2 border border-success" type="button"></button>
         </div>
     </div>
 
@@ -33,9 +33,11 @@
 </template>
 
 <script setup>
-
 </script>
 
 <style scoped>
 </style>
+
+
+
 
