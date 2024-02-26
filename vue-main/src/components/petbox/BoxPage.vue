9ab09ee45424d88eb19b1b8e03a51237f5372f63
yuan <az870517@gmail.com>
2024-02-26 22:36:18 +0800

    顯示userinfo的圖片與新增ㄧ些userinfo的資料

--- vue-main/src/components/petbox/BoxPage.vue
+++ vue-main/src/components/petbox/BoxPage.vue
@@ -1,66 +1,24 @@
-<template lang="">
+<template>
     <div class="container">
         <div class="wapper">
             <div>
                 <h1 class="fw900 text-center p-5">背包</h1>
             </div>
             <div class="content">
-                <div class="character">
-                    <div class="box_s box01_1" style="background-image: url(/box/images/c1.png);" id="c1">
-                        <div class="rank">
-                            <h5>100</h5>
+                <div class="character" id="character">
+                     <div class="box_s box01_1" :style="{backgroundImage:'url(sprites/hero/'+turn[1].id+'.png'}" v-for="turn in userInfoStore.userpetset">
+                        <div class="rank" @click="fightover">
+                            <h5>{{ turn[1].level }}</h5>
                         </div>
                     </div>
-                    <div class="box_s box01_1" style="background-image: url(/box/images/c2.png);" id="c2">
-                        <div class="rank">
-                            <h5>90</h5>
-                        </div>
-                    </div>
-                    <div class="box_s box01_1" style="background-image: url(/box/images/c3.png);" id="c3">
-                        <div class="rank">
-                            <h5>80</h5>
-                        </div>
-                    </div>
-                    <div class="box_s box01_1" style="background-image: url(/box/images/c4.png);" id="c4">
-                        <div class="rank">
-                            <h5>70</h5>
-                        </div>
-                    </div>
-                    <div class="box_s box01_1" style="background-image: url(/box/images/c5.png);" id="c5">
-                        <div class="rank">
-                            <h5>60</h5>
-                        </div>
-                    </div>
-                    <div class="box01"></div>
-                    <div class="box01"></div>
-                    <div class="box01"></div>
-                    <div class="box01"></div>
-                    <div class="box01"></div>
-                    <div class="box01"></div>
-                    <div class="box01"></div>
-                    <div class="box01"></div>
-                    <div class="box01"></div>
-                    <div class="box01"></div>
-                    <div class="box01"></div>
-                    <div class="box01"></div>
-                    <div class="box01"></div>
-                    <div class="box01"></div>
-                    <div class="box01"></div>
-                    <div class="box01"></div>
-                    <div class="box01"></div>
-                    <div class="box01"></div>
-                    <div class="box01"></div>
-                    <div class="box01"></div>
-                    <div class="box01"></div>
-                    <div class="box01"></div>
-                    <div class="box01"></div>
-                    <div class="box01"></div>
-                    <div class="box01"></div>
-                    <div class="box01"></div>
-                    <div class="box01"></div>
                 </div>
-                <div class="select d-flex align-items-center  justify-content-center" id="character_select">
-                    <h5 class="fw900 text-center align-middle text-danger">角色編成</h5>
+                <div class="select d-flex align-items-center  justify-content-center" id="character_select" @click = "add = !add">
+                    <h5 class="fw900 text-center align-middle text-danger" v-if="add">
+                        角色編成
+                    </h5>
+                    <h5 class="fw900 text-center align-middle text-danger " v-else>
+                        確定
+                    </h5>
                 </div>
                 <div class="footer">
                     <div class="put-in">
@@ -77,8 +35,13 @@
 </template>
 <script setup>
 import { useUserInfoStore } from '@/stores/userInfo';
+import { ref } from 'vue'
 
 const userInfoStore = useUserInfoStore()
-console.log(userInfoStore.username);
-
+const add = ref(true)
+console.log(userInfoStore.userpetset.get(2));
+function fightover() {
+    console.log(userInfoStore.userpetset.get(2).level);
+    h5.text='100';
+}
 </script>
