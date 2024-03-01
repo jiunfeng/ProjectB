caf1a958981f22860f88cf633ad0c3923695c3d1
Chem Yuan <az870517@gmail.com>
2024-03-01 16:35:52 +0800

    新增選取角色

--- vue-main/src/components/petbox/BoxPage.vue
+++ vue-main/src/components/petbox/BoxPage.vue
@@ -6,8 +6,9 @@
             </div>
             <div class="content">
                 <div class="character" id="character">
-                    <div class="box_s box01_1" :id=pets[0] :style="{ backgroundImage: 'url(sprites/hero/' + pets[1].id + '.png' }"
-                        v-for="pets in userInfoStore.userpets" @click="add?'':fightover(pets[1].id)">
+                    <div class="box_s box01_1" :id=pets[0]
+                        :style="{ backgroundImage: 'url(sprites/hero/' + pets[1].id + '.png' }"
+                        v-for="pets in userInfoStore.userpets" @click="add ? '' : fightover(pets[1].id)">
                         <div class="rank">
                             <h5>{{ pets[1].level }}</h5>
                         </div>
@@ -18,14 +19,23 @@
                     <h5 class="fw900 text-center align-middle text-danger" v-if="add">
                         角色編成
                     </h5>
-                    <h5 class="fw900 text-center align-middle text-danger " v-else @click = "selectchactor(userInfoStore.userpets[4][1])">
+                    <h5 class="fw900 text-center align-middle text-danger " v-else @click="selectchactor()">
                         確定
                     </h5>
                 </div>
                 <div class="footer">
                     <div class="put-in">
-                        <div class="box01_1" :style="{ backgroundImage: 'url(sprites/hero/' + (add?set[1].id:image) + '.png' }" v-for="set in userInfoStore.userpetset">
+                        <div class="box01_1" :style="{ backgroundImage: 'url(sprites/hero/' + key1 + '.png' }"
+                            @click="add ? '' : delimage(0)">
                         </div>
+                        <div class="box01_1" :style="{ backgroundImage: 'url(sprites/hero/' + key2 + '.png' }"
+                            @click="add ? '' : delimage(1)">
+                        </div>
+                        <div class="box01_1" :style="{ backgroundImage: 'url(sprites/hero/' + key3 + '.png' }"
+                            @click="add ? '' : delimage(2)">
+                        </div>
+                        <!-- <div class="box01_1" :style="{ backgroundImage: 'url(sprites/hero/' + (add?set[1].id:setimage()) + '.png' }" v-for="set in userInfoStore.userpetset">
+                        </div> -->
                         <!-- <div class="box01"></div>
                         <div class="box01"></div> -->
                     </div>
@@ -41,21 +51,58 @@ import { useUserInfoStore } from '@/stores/userInfo';
 import { ref } from 'vue'
 
 const userInfoStore = useUserInfoStore()
+var key_s = [];
+for (let keys of userInfoStore.userpetset.values()) {
+    key_s.push(keys.id);
+}
 const add = ref(true)
-const image = ref('')
 const box_s = ref('box01')
-console.log(userInfoStore.userpets);
+const key1 = ref(key_s[0])
+const key2 = ref(key_s[1])
+const key3 = ref(key_s[2])
+// function setimage(data){
+// for(let keys of userInfoStore.userpetset.values()){
+//     key_s.push(keys);
+// }
+// return key_s[data].id
+// }
+
+function delimage(data) {
+    key_s.splice(data, 1, '');
+    key1.value = key_s[0]
+    key2.value = key_s[1]
+    key3.value = key_s[2]
+}
+
+// console.log(userInfoStore.userpets);
 function fightover(data) {
-    console.log(userInfoStore.userpets);
-    image.value = data
+    if (key_s[0] == '') {
+        key_s.splice(0,1,data);
+    }else if(key_s[1] == ''){
+        key_s.splice(1,1,data);
+    }else if(key_s[2] == ''){
+        key_s.splice(2,1,data);
+    }
+    key1.value = key_s[0]
+    key2.value = key_s[1]
+    key3.value = key_s[2]
     box_s.value = 'box01_1'
 }
 
-function selectchactor(data){
+function selectchactor() {
     // console.log(data);
     // console.log(userInfoStore.userpetset.get(3));
-    console.log(userInfoStore.userpetset.get(2));
-    console.log(userInfoStore.userpets[4][1]);
-    return userInfoStore.userpetset.set(1,data),userInfoStore.userpetset.set(2,data);
+    // console.log(userInfoStore.userpetset.get(2));
+    // console.log(userInfoStore.userpets[4][1]);
+    // console.log(userInfoStore.userpetset.size);
+    console.log(key_s);
+    // userInfoStore.userpetset.set(1,userInfoStore.userpets[data1][1]);
+    // userInfoStore.userpetset.set(2,userInfoStore.userpets[data2][1]);
+    // userInfoStore.userpetset.set(3,userInfoStore.userpets[data3][1]);
+    // for(var i=0;i<userInfoStore.userpetset.size;i++){
+    //     console.log(userInfoStore.userpetset.get(i+1));
+    // }
+
+    // return userInfoStore.userpetset.set(1,userInfoStore.userpets[data1-1][1]),userInfoStore.userpetset.set(2,userInfoStore.userpets[data2-1][1]),userInfoStore.userpetset.set(3,userInfoStore.userpets[data3-1][1]);
 }
 </script>
