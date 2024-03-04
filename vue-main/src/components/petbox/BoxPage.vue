6b7c34003fb8f425b35e8476cf23cc73f36afdab
yuan <az870517@gmail.com>
2024-03-05 07:06:47 +0800

    背包功能完成

--- vue-main/src/components/petbox/BoxPage.vue
+++ vue-main/src/components/petbox/BoxPage.vue
@@ -7,19 +7,19 @@
             <div class="content">
                 <div class="character" id="character">
                     <div class="box_s box01_1" :id=pets[0]
-                        :style="{ backgroundImage: 'url(sprites/hero/' + pets[1].id + '.png' }"
-                        v-for="pets in userInfoStore.userpets" @click="add ? '' : fightover(pets[1].id)">
+                        :style="{ backgroundImage: 'url(sprites/hero/' + index + '.png' }"
+                        v-for="(pets, index) in userInfoStore.userpets" @click="add ? '' : fightover(index)">
                         <div class="rank">
-                            <h5>{{ pets['name'] }}</h5>
+                            <h5>{{ pets['level'][0] }}</h5>
                         </div>
                     </div>
                 </div>
                 <div class="select d-flex align-items-center  justify-content-center mt-auto" id="character_select"
-                    @click="add = !add">
+                    @click="add = !add , add?selectchactor():''">
                     <h5 class="fw900 text-center align-middle text-danger" v-if="add">
                         角色編成
                     </h5>
-                    <h5 class="fw900 text-center align-middle text-danger " v-else @click="selectchactor()">
+                    <h5 class="fw900 text-center align-middle text-danger " v-else >
                         確定
                     </h5>
                 </div>
@@ -50,25 +50,20 @@ import { ref } from 'vue'
 
 
 const userInfoStore = useUserInfoStore()
-var key_s = [];
+var key_id = [];
 for (let keys of userInfoStore.userpetset.values()) {
-    key_s.push(keys.id);
+    key_id.push(keys.id);
 }
+// console.log('set:'+userInfoStore.userpetset.values());
 
 console.log(key_id);
 // console.log(parseInt(key_id[1])); 使用迴圈輸出int
 
 const add = ref(true)
-const box_s = ref('box01')
-const key1 = ref(key_s[0])
-const key2 = ref(key_s[1])
-const key3 = ref(key_s[2])
-// function setimage(data){
-// for(let keys of userInfoStore.userpetset.values()){
-//     key_s.push(keys);
-// }
-// return key_s[data].id
-// }
+const key1 = ref(key_id[0])
+const key2 = ref(key_id[1])
+const key3 = ref(key_id[2])
+console.log(userInfoStore.userpetset);
 
 function delimage(data) {
     key_id.splice(data, 1, '');
@@ -78,33 +73,31 @@ function delimage(data) {
 }
 
 function fightover(data) {
-    if (key_s[0] == '') {
-        key_s.splice(0,1,data);
-    }else if(key_s[1] == ''){
-        key_s.splice(1,1,data);
-    }else if(key_s[2] == ''){
-        key_s.splice(2,1,data);
+    console.log(data);
+    if (key_id.includes(data)) {
+        key_id.splice(key_id.indexOf(data), 1, '');
+    } else {
+        if (key_id.indexOf('') == -1) {
+        } else {
+            key_id.splice(key_id.indexOf(''), 1, data);
+        }
     }
-    key1.value = key_s[0]
-    key2.value = key_s[1]
-    key3.value = key_s[2]
-    box_s.value = 'box01_1'
+    key1.value = key_id[0]
+    key2.value = key_id[1]
+    key3.value = key_id[2]
 }
 
 function selectchactor() {
-    // console.log(data);
-    // console.log(userInfoStore.userpetset.get(3));
-    // console.log(userInfoStore.userpetset.get(2));
-    // console.log(userInfoStore.userpets[4][1]);
-    // console.log(userInfoStore.userpetset.size);
-    console.log(key_s);
-    // userInfoStore.userpetset.set(1,userInfoStore.userpets[data1][1]);
-    // userInfoStore.userpetset.set(2,userInfoStore.userpets[data2][1]);
-    // userInfoStore.userpetset.set(3,userInfoStore.userpets[data3][1]);
-    // for(var i=0;i<userInfoStore.userpetset.size;i++){
-    //     console.log(userInfoStore.userpetset.get(i+1));
-    // }
+    // console.log(userInfoStore.userpetset);
+    for (var i = 0; i < 3; i++) {
+        userInfoStore.userpetset.set(i + 1, {...userInfoStore.userpets[key_id[i]], 'id':key_id[i]});
+    }
 
-    // return userInfoStore.userpetset.set(1,userInfoStore.userpets[data1-1][1]),userInfoStore.userpetset.set(2,userInfoStore.userpets[data2-1][1]),userInfoStore.userpetset.set(3,userInfoStore.userpets[data3-1][1]);
+    // console.log(key_id[1]);
+    console.log(userInfoStore.userpetset);
+    // console.log(userInfoStore.userpets[key_id[1]]);
+    // console.log(userInfoStore.userpetset.get(1));
+    // console.log(userInfoStore.userpetset.get(2));
+    // console.log(userInfoStore.userpetset.get(3));
 }
 </script>
