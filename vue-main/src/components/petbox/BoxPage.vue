00bb6546eaf41069648cc02f54be773fcc22d8d1
Chem Yuan <az870517@gmail.com>
2024-03-08 13:47:41 +0800

    交換邏輯完成,暫時用個回到主畫面按鈕

--- vue-main/src/components/petbox/BoxPage.vue
+++ vue-main/src/components/petbox/BoxPage.vue
@@ -1,8 +1,11 @@
 <template>
     <div class="container">
         <div class="wapper">
-            <div>
-                <h1 class="fw900 text-center p-5">背包</h1>
+            <div class="d-flex justify-content-end">
+                <button class="btn btn-dark" @click="changePage('MainPage')">主畫面</button>
+            </div>
+            <div class="p-3">
+                <h1 class="fw900 text-center p-3">背包</h1>
             </div>
             <div class="content">
                 <div class="character" id="character">
@@ -15,11 +18,11 @@
                     </div>
                 </div>
                 <div class="select d-flex align-items-center  justify-content-center mt-auto" id="character_select"
-                    @click="add = !add , add?selectchactor():''">
+                    @click="add = !add, add ? selectchactor() : ''">
                     <h5 class="fw900 text-center align-middle text-danger" v-if="add">
                         角色編成
                     </h5>
-                    <h5 class="fw900 text-center align-middle text-danger " v-else >
+                    <h5 class="fw900 text-center align-middle text-danger " v-else>
                         確定
                     </h5>
                 </div>
@@ -42,17 +45,22 @@
 
     <link rel="stylesheet" href="/box/css/index_box.css">
     <link rel="stylesheet" href="/box/css/bootstrap.min.css">
+    <link rel="stylesheet" href="/box/css/sweetalert2.min.css">
 </template>
+
 
 <script setup>
 import { useUserInfoStore } from '@/stores/userInfo';
 import { ref } from 'vue'
 
-
 const userInfoStore = useUserInfoStore()
+const changePage = (page) => {
+    userInfoStore.currentPage = page;
+}
 var key_id = [];
 for (let keys of userInfoStore.userpetset.values()) {
     key_id.push(keys.id);
+    console.log(userInfoStore.userpets)
 }
 // console.log('set:'+userInfoStore.userpetset.values());
 
@@ -63,7 +71,6 @@ const add = ref(true)
 const key1 = ref(key_id[0])
 const key2 = ref(key_id[1])
 const key3 = ref(key_id[2])
-console.log(userInfoStore.userpetset);
 
 function delimage(data) {
     key_id.splice(data, 1, '');
@@ -88,13 +95,17 @@ function fightover(data) {
 }
 
 function selectchactor() {
-    // console.log(userInfoStore.userpetset);
-    for (var i = 0; i < 3; i++) {
-        userInfoStore.userpetset.set(i + 1, {...userInfoStore.userpets[key_id[i]], 'id':key_id[i]});
+    if (key_id.indexOf('') == '-1') {
+        for (var i = 0; i < 3; i++) {
+            userInfoStore.userpetset.set(i + 1, { ...userInfoStore.userpets[key_id[i]], 'id': key_id[i] });
+        }
+    }else{
+        add.value=false;
+        alert("未選取三隻精靈");
     }
 
     // console.log(key_id[1]);
-    console.log(userInfoStore.userpetset);
+    //console.log(userInfoStore.userpetset);
     // console.log(userInfoStore.userpets[key_id[1]]);
     // console.log(userInfoStore.userpetset.get(1));
     // console.log(userInfoStore.userpetset.get(2));
